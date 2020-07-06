d794dac429da6e2eb85784fba0ab491c9e978246
yefu.chen <yefu.chen@zilliz.com>
2020-07-06 19:45:53 +0800

    naive test

--- scala/src/test/scala/CornerTest.scala
+++ scala/src/test/scala/CornerTest.scala
@@ -71,5 +71,32 @@ class CornerTest extends AdapterTest {
     val df3 = df2.select(st_equals('geom, 'geom).as("c"))
     df3.show()
   }
+
+  test("naive") {
+    val ss = spark
+    import ss.implicits._
+    val points_text = Seq(
+      "Point(1 1)",
+      "Point(1 2)",
+      "Point(2 1)",
+      "Point(2 2)",
+      "Point(4 5)",
+      "Point(8 8)",
+      "Point(10 10)",
+    ).toDF("points_text").withColumn("attr", monotonically_increasing_id())
+    val polygons_text = Seq(
+      "Polygon((0 0, 3 0, 3 3, 0 3, 0 0))",
+      "Polygon((6 6, 3 6, 3 3, 6 3, 6 6))",
+      "Polygon((6 6, 9 6, 9 9, 6 9, 6 6))",
+    ).toDF("polygons_text").withColumn("id", monotonically_increasing_id())
+
+    val points = points_text.select(st_geomfromtext('points_text).as("points"))
+    val polygons = polygons_text.select(st_astext(st_geomfromtext('polygons_text)).as("polygons_text_again"))
+
+    // both are unusable
+    points.show()
+    polygons.show()
+
+  }
 }
 
